905de24fc7ccfa57b680be1d765e424636652870
andrey_s <sudarev.andrey.i@gmail.com>
2023-05-21 19:06:30 +0300

    upd api-error.js

--- server/exceptions/api-error.js
+++ server/exceptions/api-error.js
@@ -1,13 +1,9 @@
 module.exports = class ApiError extends Error {
-
 	status
 	errors
-
-
 	constructor(status, message, errors = []) {
 		super(message)
 		this.status = status
-
 		this.errors = errors
 	}
 
